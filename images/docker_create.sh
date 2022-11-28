600c75fbbe95bc082c2d3db6ced30aeba3f52f73
Bet5521 <103318788+Bet5521@users.noreply.github.com>
2022-11-29 04:15:46 +0800

    Update docker_create.sh

--- images/docker_create.sh
+++ images/docker_create.sh
@@ -1,4 +1,3 @@
-
 OUT_ALERT() {
     echo -e "${CYELLOW} $1 ${CEND}"
 }
@@ -10,7 +9,7 @@ DOCKER_INSTALL() {
 
         curl -fsSL get.docker.com | bash 
     fi
-
+if false;then
     docker_compose_exists=$(docker-compose version 2>/dev/null)
     if [[ ${docker_compose_exists} == "" ]]; then
         OUT_ALERT "[?] 正在安装docker-compose"
@@ -19,6 +18,7 @@ DOCKER_INSTALL() {
         chmod +x /usr/local/bin/docker-compose && \
 	    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
     fi
+ fi   
 }
 
 
@@ -36,57 +36,73 @@ DOCKER_UP() {
         wget https://ghproxy.com/https://raw.githubusercontent.com/lu0b0/ELM/main/images/Dockerfile -O /elmmb/Dockerfile
     fi
     
-    
-    wget https://ghproxy.com/https://github.com/lu0b0/ELM/releases/download/2.4/elmmb -O /elmmb/elmmb
-    
+    if [[ ${version} != "latest" ]];then
+
+    	wget https://ghproxy.com/https://github.com/lu0b0/ELM/releases/download/$version/elmmb -O /elmmb/elmmb
+    else
+    	wget https://ghproxy.com/https://github.com/lu0b0/ELM/releases/download/$(curl -Ls "https://api.github.com/repos/lu0b0/ELM/releases/latest" | 
+	grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')/elmmb -O /elmmb/elmmb
+    fi	
     chmod -R 777 /elmmb
 	
     docker build -t='elmmb' .
 }
 
-echo -e $"\n欢迎使用饿了么登陆面板2.4 Docker一键部署脚本"
+echo -e $"\n欢迎使用饿了么登陆面板Docker一键部署脚本"
+echo -e $"使用本脚本请确认已获取萝卜授权，否则无法正常使用"
 read -p "输入Y/y确认安装 跳过安装请直接回车:  " CONFIRM
 CONFIRM=${CONFIRM:-"N"}
 
-
-##if [[ ! -f "/elmmb/Config.json"  ]]; then
-	
-##fi
 if [[ ${CONFIRM} == "Y" || ${CONFIRM} == "y" ]];then
 	if [ ! -d "/elmmb" ]; then
 		mkdir /elmmb
-	fi
-	read -p $'\n 输入授权码: ' sqm
-	sqm=${sqm:-""}
+	fi	
+		read -p $'\n 输入版本号(默认最新版)：' version
+		version=${version:-"latest"}
+		read -p "是否需要配置授权Config文件（默认配置Y）" cfg
+		cfg=${cfg:-"Y"}
+		if [[ ${cfg} == "Y" || ${cfg} == "y" ]];then
+			if [[ -f "/elmmb/Config.json"  ]]; then
+			echo -e $"\n已发现存在Config文件，是否确认重新配置？（默认N）" cfg2
+			cfg2=${cfg2:-"N"}
+				if [[ ${cfg2} == "Y" || ${cfg2} == "y" ]];then
 
-	read -p $'\n 输入青龙url: 例：（http://192.168.0.1:5700）：' qlurl
-	qlurl=${qlurl:-""}
+				read -p $'\n 输入授权码: ' sqm
+				sqm=${sqm:-""}
+				read -p $'\n 输入青龙url: 例：（http://192.168.0.1:5700）：' qlurl
+				qlurl=${qlurl:-""}
 
-	read -p $'\n 输入容器CLIENTID: ' CLIENTID
-	CLIENTID=${CLIENTID:-""}
+				read -p $'\n 输入容器CLIENTID: ' CLIENTID
+				CLIENTID=${CLIENTID:-""}
 
-	read -p $'\n 输入容器SECRET: ' SECRET
-	SECRET=${SECRET:-""}
-	
-	read -p $'\n 输入wxpusher推送的app_token 获取地址：https://wxpusher.zjiecode.com/admin (不设置推送按回车): ' wxpusher
-	wxpusher=${wxpusher:-""}
+				read -p $'\n 输入容器SECRET: ' SECRET
+				SECRET=${SECRET:-""}
+				read -p $'\n 输入容器CK数(默认40): ' NUM
+				NUM=${NUM:-"40"}	
+				read -p $'\n 输入wxpusher推送的app_token 获取地址：https://wxpusher.zjiecode.com/admin (不设置推送按回车): ' wxpusher
+				wxpusher=${wxpusher:-""}
 
-	echo "{
-		\"Authorization\":\"$sqm\",
-		\"Title\": \"饿了么\",
-		\"Announcement\": \"公告\",
-		\"wxpusher_token\":\"$wxpusher\",
-		\"Config\": [
-			{
-				\"QLkey\": 1,
-				\"QLName\": \"容器1\",
-				\"QLurl\": \"$qlurl\",
-				\"QL_CLIENTID\": \"$CLIENTID\",
-				\"QL_SECRET\": \"$SECRET\",
-				\"QL_CAPACITY\": 40
-			}
-		]
+				echo "{
+					\"Authorization\":\"$sqm\",
+					\"Title\": \"饿了么\",
+					\"Announcement\": \"公告\",
+					\"wxpusher_token\":\"$wxpusher\",
+					\"Config\": [
+								{
+									\"QLkey\": 1,
+									\"QLName\": \"容器1\",
+									\"QLurl\": \"$qlurl\",
+									\"QL_CLIENTID\": \"$CLIENTID\",
+									\"QL_SECRET\": \"$SECRET\",
+									\"QL_CAPACITY\": "$NUM"
+					}
+								]
 	}" > /elmmb/Config.json
+				fi
+			fi
+		fi
+	
+
 	DOCKER_INSTALL
 	DOCKER_UP
 fi
